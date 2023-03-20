6a6cdcdc16b1337de5d917c34b2cc643a40764ac
XxRexRaptorxX <Raptor-Simon@web.de>
2023-03-20 20:47:57 +0100

    restoring

---
+++ src/main/java/xxrexraptorxx/nexus/items/BasicItem.java
@@ -0,0 +1,12 @@
+package xxrexraptorxx.nexus.items;
+
+import net.minecraft.world.item.Item;
+
+public class BasicItem extends Item {
+
+    public BasicItem() {
+        super(new Properties()
+        );
+    }
+
+}
