04b5d2b129f92f841cfe03ca0de79a5d92cf7ed8
XxRexRaptorxX <Raptor-Simon@web.de>
2023-03-20 21:19:56 +0100

    port to MC 1.19.4

--- src/main/java/xxrexraptorxx/nexus/main/Nexus.java
+++ src/main/java/xxrexraptorxx/nexus/main/Nexus.java
@@ -34,7 +34,7 @@ public class Nexus {
     @SubscribeEvent
     public static void registerTabs(final CreativeModeTabEvent.Register event) {
         event.registerCreativeModeTab(CREATIVE_TAB, (cf) -> cf.icon(() -> new ItemStack(ModBlocks.NEXUS_GREEN.get()))
-                .title(Component.translatable("itemGroup." + References.MODID + "_tab")).displayItems((flagSet, output, ifSth) -> {
+                .title(Component.translatable("itemGroup." + References.MODID + "_tab")).displayItems((p_270258_, output) -> {
                     output.accept(ModBlocks.NEXUS_RED.get());
                     output.accept(ModBlocks.NEXUS_BLUE.get());
                     output.accept(ModBlocks.NEXUS_GREEN.get());
