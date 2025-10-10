8e9ed49fe93cb22c51ef756baa190da53a7ecc81
XxRexRaptorxX <Raptor-Simon@web.de>
2025-10-10 20:21:16 +0200

    chore: ingame config marked for the next update

--- src/main/java/xxrexraptorxx/nexus/main/Nexus.java
+++ src/main/java/xxrexraptorxx/nexus/main/Nexus.java
@@ -1,12 +1,10 @@
 package xxrexraptorxx.nexus.main;
 
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xxrexraptorxx.magmacore.config.ConfigHelper;
 import xxrexraptorxx.magmacore.main.ModRegistry;
 import xxrexraptorxx.nexus.network.ModPackets;
 import xxrexraptorxx.nexus.registries.CreativeModeTabs;
@@ -25,22 +23,23 @@ public class Nexus {
 
 
     public Nexus(IEventBus eventBus, ModContainer container) {
+        Config.init(container);
         ModBlocks.init(eventBus);
         ModItems.init(eventBus);
         CreativeModeTabs.init(eventBus);
 
         eventBus.addListener(ModPackets::register);
 
-        ConfigHelper.registerConfigs(container, References.MODID, true, Config.SERVER_CONFIG, Config.CLIENT_CONFIG);
+        //ConfigHelper.registerConfigs(container, References.MODID, true, Config.SERVER_CONFIG, Config.CLIENT_CONFIG); TODO!
         ModRegistry.register(References.MODID, References.NAME, References.URL);
     }
 
 
-    @Mod(value = References.MODID, dist = Dist.CLIENT)
-    public static class BlockDetectiveClient {
-
-        public BlockDetectiveClient(ModContainer container) {
-            ConfigHelper.registerIngameConfig(container);
-        }
-    }
+    //@Mod(value = References.MODID, dist = Dist.CLIENT)
+    //public static class BlockDetectiveClient {
+    //
+    //    public BlockDetectiveClient(ModContainer container) {
+    //        ConfigHelper.registerIngameConfig(container);
+    //    }
+    //}
 }
