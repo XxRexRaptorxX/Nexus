7d93552f2b5db9dddd044c067e2eedad5e3424a4
XxRexRaptorxX <Raptor-Simon@web.de>
2025-10-25 00:08:24 +0200

    feat: non-admin related mod items now also appear in vanilla creative tabs

--- src/main/java/xxrexraptorxx/nexus/main/Nexus.java
+++ src/main/java/xxrexraptorxx/nexus/main/Nexus.java
@@ -1,15 +1,19 @@
 package xxrexraptorxx.nexus.main;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xxrexraptorxx.magmacore.config.ConfigHelper;
 import xxrexraptorxx.magmacore.main.ModRegistry;
 import xxrexraptorxx.nexus.network.ModPackets;
-import xxrexraptorxx.nexus.registries.CreativeModeTabs;
+import xxrexraptorxx.nexus.registries.CreativeTabs;
 import xxrexraptorxx.nexus.registries.ModBlocks;
 import xxrexraptorxx.nexus.registries.ModItems;
 import xxrexraptorxx.nexus.utils.Config;
@@ -24,12 +28,13 @@ public class Nexus {
     public static final Logger LOGGER = LogManager.getLogger();
 
 
-    public Nexus(IEventBus eventBus, ModContainer container) {
-        ModBlocks.init(eventBus);
-        ModItems.init(eventBus);
-        CreativeModeTabs.init(eventBus);
+    public Nexus(IEventBus bus, ModContainer container) {
+        ModBlocks.init(bus);
+        ModItems.init(bus);
+        CreativeTabs.init(bus);
 
-        eventBus.addListener(ModPackets::register);
+        bus.addListener(ModPackets::register);
+        bus.addListener(this::addCreative);
 
         ConfigHelper.registerConfigs(container, References.MODID, false, Config.SERVER_CONFIG, null);
         ModRegistry.register(References.MODID, References.NAME, References.URL);
@@ -41,6 +46,19 @@ public class Nexus {
 
         public BlockDetectiveClient(ModContainer container) {
             ConfigHelper.registerIngameConfig(container);
+        }
+    }
+
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        ResourceKey<CreativeModeTab> key = event.getTabKey();
+
+        if (key == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TRANSMITTER);
+
+        } else if (key == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.NEXUS_TRACKER);
+            event.accept(ModItems.REPAIR_KIT);
         }
     }
 }
--- src/main/java/xxrexraptorxx/nexus/registries/CreativeTabs.java
+++ src/main/java/xxrexraptorxx/nexus/registries/CreativeTabs.java
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import xxrexraptorxx.nexus.main.References;
 
-public class CreativeModeTabs {
+public class CreativeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, References.MODID);
 
